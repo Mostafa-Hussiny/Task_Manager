fec3c2e94339e5e7e377a63a95eb15f9a41eb413
Mostafa <hussinym2@gmail.com>
2025-04-20 14:48:19 +0200

    Error_Handling_and_port_variable

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
 require('dotenv').config()
 const notFound = require('./middleware/not_found')
+const errorHandlerMiddleware = require('./middleware/error_handler')
 
 
 // middleware
@@ -14,12 +15,15 @@ app.use(express.json());
 app.use('/api/v1/tasks', tasks)
 
 app.use(notFound)
+app.use(errorHandlerMiddleware)
+
+const port = process.env.PORT || 3000;
 
 const start = async ()=> {
     try {
         await connectDB(process.env.MONGO_URI)
-        app.listen(3000, () => {
-            console.log('server is listening on port 3000');
+        app.listen(port, () => {
+            console.log(`server is listening on port ${port}..`);
         })
     } catch (error) {
         console.log(error)
